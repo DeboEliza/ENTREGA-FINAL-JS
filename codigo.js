1e0e22ed32aac49d4708e456f575438277b7f8d2
deboeliza <deboeliza2015@gmail.com>
2022-05-11 11:28:50 -0300

    mejorando entrega3

--- codigo.js
+++ codigo.js
@@ -13,44 +13,56 @@
 
 
 // calcuraremos un precio de la indumentaria
-let nombreProducto = prompt ("Ingresa Nombre Producto")
-alert ("Igresaste=" + nombreProducto);
-console.log ("Tu producto - " + nombreProducto);
+//let nombreProducto = prompt ("Ingresa Nombre Producto")
+//alert ("Igresaste=" + nombreProducto);
+//console.log ("Tu producto - " + nombreProducto);
 
-let colorDelProducto = prompt ("Ingresa color del Producto")
-alert ("Ingresaste=" + colorDelProducto);
-console.log ("tu color - " + colorDelProducto);
+//let colorDelProducto = prompt ("Ingresa color del Producto")
+//alert ("Ingresaste=" + colorDelProducto);
+//console.log ("tu color - " + colorDelProducto);
 
-let TallaProducto = prompt ("Ingresa Talla del Producto")
-alert ("Ingresaste=" + TallaProducto);
-console.log ("tu talla - "  + TallaProducto);
+//let TallaProducto = prompt ("Ingresa Talla del Producto")
+//alert ("Ingresaste=" + TallaProducto);
+//console.log ("tu talla - "  + TallaProducto);
 
-function calcularPrecioConDescuento (){
-    let precioProducto = (parseFloat ( prompt ("Ingrese el precio del producto")));
-    let precioFinal = precioProducto * 0.5 ;
-    console.log ( "En efectivo con Descuento $" + precioFinal);
-}
+//function calcularPrecioConDescuento (){
+  //  let precioProducto = (parseFloat ( prompt ("Ingrese el precio del producto")));
+  //  let precioFinal = precioProducto * 0.5 ;
+  //  console.log ( "En efectivo con Descuento $" + precioFinal);
+//}
 
-calcularPrecioConDescuento ();
+//calcularPrecioConDescuento ();
 
-function pollera (tela,largo,estilo){
+function Pollera (tela,largo,estilo){
      this.tela = tela;
      this.largo = largo;
      this.estilo = estilo;
 
-  this.mostrarTela = function(){connsole.log ("Tela: " + this.tela)};
-  this.mostrarLargo = function(){connsole.log ("Largo: " + this.largo)};
-  this.mostrarEstilo = function(){connsole.log ("Estilo: " + this.Estilo)};
+  this.mostrarTela = function() {console.log ("Tela: " + this.tela)};
+  this.mostrarLargo = function() {console.log ("Largo: " + this.largo)};
+  this.mostrarEstilo = function() {console.log ("Estilo: " + this.estilo)};
 }
 
-const pollera1= new pollera ("Lanilla","Larga","acampanada");
-const pollera2= new pollera ("Seda","Corta","Recta");
+const Pollera1= new Pollera ("Lanilla","Larga", "Acampanada");
+const Pollera2= new Pollera ("Seda","Corta", "Recta");
 
-pollera1.mostrarTela();
-pollera1.mostrarLargo();
-pollera1.mostrarEstilo();
+console.log("FALDA 1")
+Pollera1.mostrarTela();
+Pollera1.mostrarLargo();
+Pollera1.mostrarEstilo();
 
-pollera2.mostrarTela();
-pollera2.mostrarTela();
-pollera2.mostrarTela();
+console.log("FALDA 2")
+Pollera2.mostrarTela();
+Pollera2.mostrarLargo();
+Pollera2.mostrarEstilo();
 
+console.log ("Hola Cual es tu Falda Favorita?")
+let marca = prompt ("Ingresa la marca que te gusta");
+let tela = prompt ("Ingresa tipo de tela que te gusta");
+let estilo = prompt("Ingresa el estilo que te gusta");
+
+console.log ("te gusta - "  + marca);
+console.log ("te gusta - "  + tela);
+console.log ("te gusta - "  + estilo);
+
+console.log( "GENIAL!! GRACIAS POR TU OPINION!")
